40a94e9e055213530765c799c0f63a068ee0f0ef
Alex <alex.s.wheaton@gmail.com>
2019-10-31 23:58:37 +0000

    Added code for several integration schemes.

--- fig_2_3.py
+++ fig_2_3.py
@@ -1,16 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def analytic_soln(NX=50,v_d=0.001,n_max=25,t=1):
-	x = np.linspace(0.0, 1.0, NX)
-	w = np.zeros(NX)
-	for n in range(n_max):
-		w += 2.0 * np.sin(n * np.pi / 2)* np.sin(np.pi * n * x) * np.exp(-np.pi**2.0 * n**2.0 * v_d * t)
-	return(x, w)
+def analytic_soln(x, t, v_d=0.001, n_max=25):
+	soln = 0.0
+	for n in range(1, n_max):
+		soln += 2.0 * np.sin(n * np.pi / 2)* np.sin(np.pi * n * x) * np.exp(-np.pi**2.0 * n**2.0 * v_d * t)
+	return(soln)
 
-# Return the forward difference approximation for the space derivative of an
-# arbitrary discretized function, for some interval delta_x, and about the
-# spatial grid point j.
-def 2d_forward_time_diff():
-    func_next = func + (v_d * delta_t / delta_x**2.0) * (func_next + func_last - 2 * func)
-    return(func_next)
+# Return the forward difference approximation in time for a spatially dependent
+# function u(x), for some discretization delta_x, and time interval delta_t for
+# all spatial grid points j.
+def forward_time_diff(u_num, delta_x, delta_t, v_d=0.001):
+	u_num_next = []
+	for j in range(len(u_num)):
+		if j != 0 and j != (len(u_num) - 1):
+			u_num_next.append(u_num[j]
+			+ (v_d * delta_t / delta_x**2.0)
+			* (u_num[j+1] + u_num[j-1] - 2.0 * u_num[j]))
+		else: # Boundary condition always sets first and last item of u to 0.0.
+			u_num_next.append(0.0)
+	return(np.array(u_num_next))
+
+def main():
+
+	# Initial conditions and parameters for the integration.
+	x_start = 0.0
+	x_stop = 1.5
+	delta_x = 0.002
+
+	# Plot the analytical solution at t=1.0 and t=3.0.
+	positions = np.linspace(x_start, x_stop, num=((x_stop - x_start) / delta_x))
+	u_analytic_t1 = analytic_soln(positions, 1.0)
+	u_analytic_t3 = analytic_soln(positions, 3.0)
+	plt.plot(positions, u_analytic_t1, label='t=1.0, Analytical')
+	plt.plot(positions, u_analytic_t3, label='t=3.0, Analytical')
+
+	# Perform the integration from t=1.0 to t=3.0, for various delta_t and plot
+	# the results.
+	t_start, t_stop = 1.0, 3.0
+	for delta_t in [0.2,0.1,0.05]:
+		# Advance the numerical solution to some smooth state analytically.
+		u_numerical = analytic_soln(positions, t_start)
+
+		# Compute the rest numerically by calling the forward_time_diff function.
+		for t in np.linspace(t_start, t_stop, num=((t_stop - t_start) / delta_t)):
+			u_numerical = forward_time_diff(u_numerical, delta_x, delta_t)
+		errors = abs(u_numerical - analytic_soln(positions, t_stop)) / analytic_soln(positions, t_stop)
+		plt.plot(positions, u_numerical, label='t=3.0, $\Delta$t='+str(delta_t))
+		plt.plot(positions, errors, label='$\epsilon$, t=3.0, $\Delta$t='+str(delta_t))
+
+	plt.legend()
+	plt.show()
+
+	# Initial conditions and parameters for the integration.
+	# delta_t = 0.2
+	# delta_x = [0.02, 0.01, 0.005]
+
+main()
---
+++ fig_2_4.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Return the forward difference approximation for a function at a given point
+# for a given differential equation and timestep.
+def forward_time_diff(func, diff_eq, delta_t):
+    func_next = func + diff_eq * delta_t
+    return(func_next)
+
+# Return the forward difference approximation for a function at a given point
+# for a given differential equation and timestep.
+def backward_time_diff(func, diff_eq_next, delta_t):
+    func_next = func + diff_eq_next * delta_t
+    return(func_next)
+
+# Return the forward difference approximation for a function at a given point
+# for a given differential equation and timestep.
+def centered_time_diff(func, diff_eq, diff_eq_next, delta_t):
+    func_next = func + 0.5 * (diff_eq + diff_eq_next) * delta_t
+    return(func_next)
+
+def scheme_233(func_last, diff_eq, delta_t):
+    func_next = 2.0 * delta_t * diff_eq + func_last
+    return(func_next)
+
+def main():
+
+    fig, ax = plt.subplots(2, 2)
+
+    # Initial conditions and parameters for the integration.
+    delta_t = 1.5
+    time_n = np.linspace(0.0, 12.0, num=12.0/delta_t)
+    forw_int_func_t15, back_int_func_t15, cent_int_func_t15 = [1.0], [1.0], [1.0]
+
+    # Call each integration method at each timestep.
+    for t in time_n[1:]:
+        u_current = forw_int_func_t15[-1]
+        diff_eq_current = - u_current
+        forw_int_func_t15.append(forward_time_diff(u_current, diff_eq_current, delta_t))
+
+        u_current = back_int_func_t15[-1]
+        diff_eq_next = - u_current / (1.0 + delta_t)
+        back_int_func_t15.append(backward_time_diff(u_current, diff_eq_next, delta_t))
+
+        u_current = cent_int_func_t15[-1]
+        diff_eq_current = - u_current
+        diff_eq_next = - (u_current + (-u_current * delta_t / 2.0)) / (1.0 + delta_t)
+        cent_int_func_t15.append(centered_time_diff(u_current, diff_eq_current, diff_eq_next, delta_t))
+
+    ax[0][0].plot(time_n, forw_int_func_t15)
+    ax[0][0].plot(time_n, back_int_func_t15)
+    ax[0][0].plot(time_n, cent_int_func_t15)
+
+    delta_t = 2.05
+    time_n = np.linspace(0.0, 12.0, num=12.0/delta_t)
+    forw_int_funct_205, back_int_funct_205, cent_int_funct_205 = [1.0], [1.0], [1.0]
+
+    # Call each integration method at each timestep.
+    for t in time_n[1:]:
+        u_current = forw_int_funct_205[-1]
+        diff_eq_current = - u_current
+        forw_int_funct_205.append(forward_time_diff(u_current, diff_eq_current, delta_t))
+
+        u_current = back_int_funct_205[-1]
+        diff_eq_next = - u_current / (1.0 + delta_t)
+        back_int_funct_205.append(backward_time_diff(u_current, diff_eq_next, delta_t))
+
+        u_current = cent_int_funct_205[-1]
+        diff_eq_current = - u_current
+        diff_eq_next = - (u_current + (-u_current * delta_t / 2.0)) / (1.0 + delta_t)
+        cent_int_funct_205.append(centered_time_diff(u_current, diff_eq_current, diff_eq_next, delta_t))
+
+    ax[0][1].plot(time_n, forw_int_funct_205)
+    ax[0][1].plot(time_n, back_int_funct_205)
+    ax[0][1].plot(time_n, cent_int_funct_205)
+
+    delta_t = 0.45
+    time_n = np.linspace(0.0, 12.0, num=12.0/delta_t)
+    scheme_233_int_func_t045 = [1.0, np.exp(-time_n[1])]
+
+    # Call each integration method at each timestep. Calculate and record errors.
+    for t in time_n[2:]:
+        u_last = scheme_233_int_func_t045[-2]
+        diff_eq_current = -scheme_233_int_func_t045[-1]
+        scheme_233_int_func_t045.append(scheme_233(u_last, diff_eq_current, delta_t))
+
+    ax[1][0].plot(time_n, scheme_233_int_func_t045)
+
+    delta_t = 0.045
+    time_n = np.linspace(0.0, 12.0, num=12.0/delta_t)
+    scheme_233_int_func_t0045 = [1.0, np.exp(-time_n[1])]
+
+    # Call each integration method at each timestep. Calculate and record errors.
+    for t in time_n[2:]:
+        u_last = scheme_233_int_func_t0045[-2]
+        diff_eq_current = -scheme_233_int_func_t0045[-1]
+        scheme_233_int_func_t0045.append(scheme_233(u_last, diff_eq_current, delta_t))
+
+    ax[1][1].plot(time_n, scheme_233_int_func_t0045)
+
+    plt.show()
+main()
